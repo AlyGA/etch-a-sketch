633c96c6bef9d43b8064b5ac1b6bb4044ecb2be9
Aly Anderson <aly779372@gmail.com>
2022-03-13 17:01:30 -0700

    Update files to add a square grid

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Add Javascript soon
+
+// Initial values
+const container = document.querySelector(".container");
+
+const content = document.createElement("div");
+
+// Add classes to div and then append to container div
+content.classList.add("content");
+container.appendChild(content);
