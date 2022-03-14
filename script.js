a5dfa69339e32ffb62416e71ae56c38b10cf74c3
Aly Anderson <aly779372@gmail.com>
2022-03-14 14:20:58 -0700

    Create button to make grid restart

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-// Add Javascript soon
-
 // Initial values
 const grid = document.querySelector(".grid");
-let row;
-let column;
-let cellCount = 25;
+const restartButton = document.getElementById("restart");
+let row = null;
+let column = null;
 
-// Loops through the cellCount's array and adds a new div each time to make a grid.
-for (let i = 0; i < cellCount; i++) {
-  row = document.createElement("div");
-  row.classList.add("row");
+const playGame = function (cellCount) {
+  cellCount = Number(prompt("How many cells? Max is 100."));
+  // Loops through the cellCount's array and adds a new div each time to make a grid.
+  if (cellCount <= 100) {
+    for (let i = 0; i < cellCount; i++) {
+      row = document.createElement("div");
+      row.classList.add("row");
 
-  // Add a column of divs to each row of divs. Make sure there's no overlap.
-  for (let j = 0; j < cellCount - 3; j++) {
-    column = document.createElement("div");
-    column.classList.add("column");
-    row.appendChild(column);
+      // Add a column of divs to each row of divs. Make sure there's no overlap.
+      for (let j = 0; j < cellCount - 3; j++) {
+        column = document.createElement("div");
+        column.classList.add("column");
+        row.appendChild(column);
 
-    // Add function to leave color once a user hovers over the grid
-    column.addEventListener("mouseenter", function () {
-      this.classList.add("column-hover");
-      this.classList.remove("column");
-    });
+        // Add function to leave color once a user hovers over the grid
+        column.addEventListener("mouseenter", function () {
+          this.classList.add("column-hover");
+          this.classList.remove("column");
+        });
+      }
+      grid.appendChild(row);
+    }
+  } else {
+    alert("Number is too high! Please try again!");
+    playGame();
   }
-  grid.appendChild(row);
-}
+};
+
+playGame();
+
+// Restarting game when button is pressed
+restartButton.addEventListener("click", () => {
+  document.querySelectorAll(".row").forEach((e) => e.parentNode.removeChild(e));
+
+  playGame();
+});
