62d0d56eb686c01761015aabac30cb44f517f869
Aly Anderson <aly779372@gmail.com>
2022-03-14 11:50:11 -0700

    Add function to create hover effect in js

--- script.js
+++ script.js
@@ -2,20 +2,26 @@
 
 // Initial values
 const grid = document.querySelector(".grid");
-const rowContainer = document.querySelector(".row-container");
-const columnContainer = document.querySelector(".column-container");
+let row;
+let column;
 let cellCount = 25;
 
 // Loops through the cellCount's array and adds a new div each time to make a grid.
 for (let i = 0; i < cellCount; i++) {
-  const row = document.createElement("div");
+  row = document.createElement("div");
   row.classList.add("row");
 
   // Add a column of divs to each row of divs. Make sure there's no overlap.
   for (let j = 0; j < cellCount - 3; j++) {
-    const column = document.createElement("div");
+    column = document.createElement("div");
     column.classList.add("column");
     row.appendChild(column);
+
+    // Add function to leave color once a user hovers over the grid
+    column.addEventListener("mouseenter", function () {
+      this.classList.add("column-hover");
+      this.classList.remove("column");
+    });
   }
   grid.appendChild(row);
 }
